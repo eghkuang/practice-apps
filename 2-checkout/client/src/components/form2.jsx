2bb072a6d03ba5aa4c18577cf020ca600f9e0ff1
Esther Kuang <eghkuang@gmail.com>
2022-07-13 22:46:29 -0700

    re-clone

--- 2-checkout/client/src/components/form2.jsx
+++ 2-checkout/client/src/components/form2.jsx
@@ -6,10 +6,15 @@ function Form2({state, form2, handleForm}) {
         <h2>whereabouts/Form2:</h2>
       <label>
         <input onChange={handleForm} value={state.address1} type="text" name="address1" placeholder="Address 1"/>
+
         <input onChange={handleForm} value={state.address2} type="text" name="address2" placeholder="Address 2"/>
+
         <input onChange={handleForm} value={state.city} type="text" name="city" placeholder="City"/>
+
         <input onChange={handleForm} value={state.state} type="text" name="state" placeholder="State"/>
+
         <input onChange={handleForm} value={state.addressZip} type="text" name="addressZip" placeholder="Zip Code"/>
+
         <input onChange={handleForm} value={state.phone} type="text" name="phone" placeholder="Phone Number"/>
       </label>
       <input type="submit" value="NEXT"/>
